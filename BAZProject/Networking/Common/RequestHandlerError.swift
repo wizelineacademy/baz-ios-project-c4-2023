6d77886d7dbed207e55a0534b965eac92c28cdeb
Grecia Escarcega <grecia.escarcega@bancoazteca.com>
2023-03-30 14:26:59 -0600

    Se agregan descripciones a errores de request handler

--- BAZProject/Networking/Common/RequestHandlerError.swift
+++ BAZProject/Networking/Common/RequestHandlerError.swift
@@ -8,5 +8,13 @@
 import Foundation
 
 enum RequestHandlerError: LocalizedError {
-    case requestBuilder, nullData, serviceError(NSError), parsing
+    case requestBuilder, nullData, serviceError(NSError)
+    
+    var errorDescription: String? {
+        switch self {
+        case .requestBuilder: return NSLocalizedString("Could not build request from provided URL", comment: "Request Error")
+        case .nullData: return NSLocalizedString("Received data is nil", comment: "No Data Received")
+        case .serviceError(let error): return error.localizedDescription
+        }
+    }
 }
