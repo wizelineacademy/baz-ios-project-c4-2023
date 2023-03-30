f3628772480f7c064c3ec6299cad5821e092c9f0
Juan Manuel García Carmona <juan.garciaca@elektra.com.mx>
2023-03-30 11:36:35 -0600

    Se agrega estructura MovieList para poder decodificar la respuesta del endpoint de la lista de películas

--- BAZProject/Models/Movie.swift
+++ BAZProject/Models/Movie.swift
@@ -6,7 +6,11 @@
 
 import Foundation
 
-struct Movie {
+struct MovieList: Codable {
+    let results: [Movie]
+}
+
+struct Movie: Codable {
     let id: Int
     let title: String
     let poster_path: String
