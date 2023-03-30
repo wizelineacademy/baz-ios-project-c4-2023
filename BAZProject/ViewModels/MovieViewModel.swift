631d456a59bad5e0ed7ebc4b4b6d19797abc908e
Juan Manuel García Carmona <juan.garciaca@elektra.com.mx>
2023-03-30 10:54:49 -0600

    Se crea MovieViewModel para desacoplar la información de la vista

---
+++ BAZProject/ViewModels/MovieViewModel.swift
@@ -0,0 +1,47 @@
+//
+//  MovieViewModel.swift
+//  BAZProject
+//
+//  Created by jmgarciaca on 30/03/23.
+//
+
+import Foundation
+
+struct MovieListViewModel {
+    let movies: [Movie]
+}
+
+extension MovieListViewModel {
+    var numberOfSections: Int {
+        return 1
+    }
+    
+    func numberOfRowsInSection(_ section: Int) -> Int {
+        return self.movies.count
+    }
+    
+    func movieAtIndex(_ index: Int) -> MovieViewModel {
+        let movie = self.movies[index]
+        return MovieViewModel(movie)
+    }
+}
+
+struct MovieViewModel {
+    private let movie:Movie
+}
+
+extension MovieViewModel {
+    init(_ movie: Movie) {
+        self.movie = movie
+    }
+}
+
+extension MovieViewModel {
+    var title: String {
+        return self.movie.title
+    }
+    
+    var poster_path: String {
+        return "https://image.tmdb.org/t/p/w500/\(self.movie.poster_path)"
+    }
+}
