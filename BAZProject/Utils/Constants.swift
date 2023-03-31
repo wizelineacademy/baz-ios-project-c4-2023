5dcd80cb91e3e935f9bfcbca90c08884e38781c9
Juan Manuel García Carmona <juan.garciaca@elektra.com.mx>
2023-03-30 21:13:57 -0600

    Se agrega la clase Constants para obtener los paths de las llamadas al webservice

---
+++ BAZProject/Utils/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  BAZProject
+//
+//  Created by jmgarciaca on 30/03/23.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Paths {
+        static let urlForTrendingMovieDay = "https://api.themoviedb.org/3/trending/movie/day?api_key=f6cd5c1a9e6c6b965fdcab0fa6ddd38a"
+    }
+    
+}
