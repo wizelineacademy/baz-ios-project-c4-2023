95c67a126a3fd8e6607b32286fd3ce8a61d77d7e
Joel Hernández G <joelhernandezg77@gmail.com>
2023-04-11 15:19:20 -0600

    [MODIFY] - Add comments to a loadImage function

--- BAZProject/Utils/ImageLoader.swift
+++ BAZProject/Utils/ImageLoader.swift
@@ -17,10 +17,15 @@ struct ImageLoader: ImageLoadable {
     
     private let global = DispatchQueue.global()
     
-    ///Function to transform the url image to a UIImage and return it
-    ///Parama url: The url image
-    ///Param callBack: The completion whit image
-    ///Return the image from data
+    /**
+        Function to transform the url image to a `UIImage` and return it
+      - Parameters:
+        - url: The url image.
+        - callBack: Returns image Data.
+      - Returns: The image from data.
+      - Note:
+      - Version: 1.0.0
+    */
     func loadImage(from url: URL, callBack: @escaping (UIImage?) -> Void) {
         global.async {
             let data = try? Data(contentsOf: url)
