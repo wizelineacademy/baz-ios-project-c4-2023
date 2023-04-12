0e9677732e24fb17d7963d6663037e8c1671a306
Juan Manuel García Carmona <juan.garciaca@elektra.com.mx>
2023-04-12 11:33:34 -0600

    Se agregan nuevos path y se renombre case trending

--- BAZProject/Networking/Endpoint.swift
+++ BAZProject/Networking/Endpoint.swift
@@ -11,14 +11,30 @@ enum Endpoint {
     static let base = "https://api.themoviedb.org/3"
     static let apiKey = "?api_key=f6cd5c1a9e6c6b965fdcab0fa6ddd38a"
     
-    case trendingMovieDay
+    case trending
+    case nowPlaying
+    case popular
+    case topRated
+    case upcoming
 }
 
 extension Endpoint {
     var url: URL? {
+        var path = ""
+        
         switch self {
-        case .trendingMovieDay:
-            return URL(string: "\(Endpoint.base)/trending/movie/day\(Endpoint.apiKey)")
+        case .trending:
+            path = "/trending/movie/day"
+        case .nowPlaying:
+            path = "/movie/now_playing"
+        case .popular:
+            path = "/movie/popular"
+        case .topRated:
+            path = "/movie/top_rated"
+        case .upcoming:
+            path = "/movie/upcoming"
         }
+        
+        return URL(string: "\(Endpoint.base)\(path)\(Endpoint.apiKey)")
     }
 }
