24ae8c8379f1a83b10f91ddfdd539e28653be7e7
Juan Manuel García Carmona <juan.garciaca@elektra.com.mx>
2023-04-17 23:55:56 -0600

    Se termina la estructura ImageLoader que se usa para cargar imágenes de forma sincrona

--- BAZProject/Resources/Utils/ImageLoader.swift
+++ BAZProject/Resources/Utils/ImageLoader.swift
@@ -4,3 +4,25 @@
 //
 //  Created by jmgarciaca on 31/03/23.
 //
+
+import Foundation
+import UIKit
+
+struct ImageLoader {
+    private let queue = DispatchQueue(label: "com.imageQueue")
+    
+    func getImage(from url: URL, completion: @escaping (UIImage) -> Void) {
+        
+        queue.async {
+            if let data = try? Data(contentsOf: url), let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    completion(image)
+                }
+            } else if let image = UIImage(named: "poster") {
+                DispatchQueue.main.async {
+                    completion(image)
+                }
+            }
+        }
+    }
+}
