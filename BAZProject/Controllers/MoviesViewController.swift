2d1e55bfcaf4902017773b844fe2e99e90bdd39b
Juan Manuel García Carmona <juan.garciaca@elektra.com.mx>
2023-04-18 01:43:26 -0600

    Se tunea el codigo para separar las funcionalidades bajo el principio de responsabilidad única, se implementan nuevos nombres de funciones

--- BAZProject/Controllers/MoviesViewController.swift
+++ BAZProject/Controllers/MoviesViewController.swift
@@ -9,38 +9,56 @@ import UIKit
 
 final class MoviesViewController: MasterMovieTableViewController {
     
+    @IBOutlet weak var btnFilter: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setup(MovieListViewModel(.trending))
+        
+        setupViewModel(MovieListViewModel(.trending))
+        setupView()
+        applyFilter()
     }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 }
 
 // MARK: - Methods
 
 extension MoviesViewController {
     
-    
-    /**
-     Configures the view controller by loading trending movies for the day and populating the table view with the results.
-    */
-//    private func setup() {
-//
-//
-//
-//    }
-    
-}
+    func setupView() {
+        var menuItems: [UIAction] {
+            return [
+                UIAction(title: "Treanding",   handler: { [weak self] _ in self?.applyFilter(.trending)   }),
+                UIAction(title: "Now Playing", handler: { [weak self] _ in self?.applyFilter(.nowPlaying) }),
+                UIAction(title: "Popular",     handler: { [weak self] _ in self?.applyFilter(.popular)    }),
+                UIAction(title: "Top Rated",   handler: { [weak self] _ in self?.applyFilter(.topRated)   }),
+                UIAction(title: "Upcoming",    handler: { [weak self] _ in self?.applyFilter(.upcoming)   })
+            ]
+        }
 
-extension MoviesViewController: MasterMovieTableViewProtocols {
-    func setup(_ viewModel: MovieListViewModel) {
-        self.viewModel = viewModel
-        
-        viewModel.filter() { [weak self] in
-            self?.navigationItem.title = viewModel.getTitle()
+        btnFilter.menu = UIMenu(title: "Filter", image: nil, identifier: nil, options: [], children: menuItems)
+    }
+    
+    func applyFilter(_ filterType: Enum_MovieFilters? = nil) {
+        viewModel?.applyFilter(filterType) { [weak self] in
+            self?.navigationItem.title = self?.viewModel?.getTitle()
             self?.tableView.reloadData()
         }
     }
+}
+
+extension MoviesViewController: MasterMovieTableViewProtocols {
     
-    
-    
+    func setupViewModel(_ viewModel: MovieListViewModel) {
+        self.viewModel = viewModel
+    }
 }
