fe7a3e4d39ee05f833e324ea9a913d5273125c20
Juan Manuel García Carmona <juan.garciaca@elektra.com.mx>
2023-04-17 23:58:39 -0600

    Se agrega UIViewController+Extensions para tunear la apariencia del UIViewController para manipular el background y el title

---
+++ BAZProject/Resources/Extensions/UIViewController+Extensions.swift
@@ -0,0 +1,42 @@
+//
+//  UIViewController+Extensions.swift
+//  BAZProject
+//
+//  Created by jmgarciaca on 17/04/23.
+//
+
+import UIKit
+
+extension UIViewController {
+
+    /*
+     * Fuente: https://newbedev.com/ios13-navigation-bar-large-titles-not-covering-status-bar
+     */
+    func configureNavigationBar(largeTitleColor: UIColor, backgoundColor: UIColor, tintColor: UIColor, title: String, preferredLargeTitle: Bool) {
+        if #available(iOS 13.0, *) {
+            let navBarAppearance = UINavigationBarAppearance()
+            navBarAppearance.configureWithOpaqueBackground()
+            navBarAppearance.largeTitleTextAttributes = [.foregroundColor: largeTitleColor]
+            navBarAppearance.titleTextAttributes = [.foregroundColor: largeTitleColor]
+            navBarAppearance.backgroundColor = backgoundColor
+
+            navigationController?.navigationBar.standardAppearance = navBarAppearance
+            navigationController?.navigationBar.compactAppearance = navBarAppearance
+            navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+
+            navigationController?.navigationBar.prefersLargeTitles = preferredLargeTitle
+            navigationController?.navigationBar.isTranslucent = false
+            navigationController?.navigationBar.tintColor = tintColor
+            navigationItem.title = title
+
+        } else {
+            // Fallback on earlier versions
+            navigationController?.navigationBar.barTintColor = backgoundColor
+            navigationController?.navigationBar.tintColor = tintColor
+            navigationController?.navigationBar.isTranslucent = false
+            navigationItem.title = title
+        }
+    }
+    
+}
+
