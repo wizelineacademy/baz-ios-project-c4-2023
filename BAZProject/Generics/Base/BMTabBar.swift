cb1897260c491ea7335266b0cafcd979394b9be9
Benjamín Velazquez <benjamin.velazquez@elektra.com.mx>
2023-04-19 10:44:01 -0600

    preview move View Trending

---
+++ BAZProject/Generics/Base/BMTabBar.swift
@@ -0,0 +1,29 @@
+//
+//  BMTabBar.swift
+//  BAZProject
+//
+//  Created by bvelazquezva on 19/04/23.
+//
+
+import UIKit
+
+class BMTabBar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
