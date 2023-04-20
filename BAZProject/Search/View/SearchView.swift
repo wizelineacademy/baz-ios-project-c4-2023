910ea8acbb3732746470196355bc8b543c996403
Benjamín Velazquez <benjamin.velazquez@elektra.com.mx>
2023-04-20 17:49:25 -0600

    Add serachbar

--- BAZProject/Search/View/SearchView.swift
+++ BAZProject/Search/View/SearchView.swift
@@ -10,15 +10,60 @@ import Foundation
 import UIKit
 
 class SearchView: UIViewController {
-
+    
+    @IBOutlet weak var schBar: UISearchBar!
+    
+    @IBOutlet weak var tblContent: UITableView!
+    
     // MARK: Properties
     var presenter: SearchPresenterProtocol?
 
     // MARK: Lifecycle
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         presenter?.viewDidLoad()
+    }
+}
+
+// MARK: - TableView's DataSource
+
+extension SearchView: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        entity?.result?.count ?? 0
+        0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: TrendingTableViewCell.identifier, for: indexPath) as? TrendingTableViewCell{
+            return cell
+        }else{
+            let cell = UITableViewCell()
+            return cell
+        }
+    }
+
+}
+
+// MARK: - TableView's Delegate
+
+extension SearchView: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        var config = UIListContentConfiguration.cell()
+        config.text = entity?.result?[indexPath.row].title
+        config.image = UIImage(named: "poster")
+        cell.contentConfiguration = config
+    }
+
+}
+
+// MARK: - SearchBar's Delegate
+extension SearchView: UISearchBarDelegate{
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        <#code#>
     }
 }
 
