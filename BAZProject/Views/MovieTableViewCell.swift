80639c273621a3a67e60d4833df5823b6cebbc39
Juan Manuel García Carmona <juan.garciaca@elektra.com.mx>
2023-04-20 21:39:57 -0600

    Se crea el protocolo MovieTableViewCellProtocols en la clase MovieTableViewCell

--- BAZProject/Views/MovieTableViewCell.swift
+++ BAZProject/Views/MovieTableViewCell.swift
@@ -8,28 +8,30 @@
 import UIKit
 
 protocol MovieTableViewCellProtocols {
-    func didTapCell()
+    func didTapCell(_ movieVM: MovieViewModel?)
 }
 
 class MovieTableViewCell: UITableViewCell {
+    var delegate:MovieTableViewCellProtocols!
     
     private let imageLoader: ImageLoader = ImageLoader()
+    var movieVM: MovieViewModel?
     
+    @IBOutlet weak var view: UIView!
     @IBOutlet weak var imgView: UIImageView!
-    
     @IBOutlet weak var lblDescription: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
         self.accessoryType = .disclosureIndicator
+        
+        view.isUserInteractionEnabled = true
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(didTap)))
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-        print("Hola!!!")
     }
     
 }
@@ -39,11 +41,19 @@ extension MovieTableViewCell {
     func setup(_ movieVM:MovieViewModel) {
         lblDescription.text = movieVM.title
         imgView.image = UIImage(named: "poster")
+        self.movieVM = movieVM
         
-        if let url = URL(string: movieVM.poster_path) {
+        if let url = URL(string: movieVM.poster_path ?? "") {
             imageLoader.getImage(from: url) { [weak self] image in
                 self?.imgView.image = image
             }
         }
     }
+
+    @objc func didTap () {
+        if self.delegate != nil {
+            self.delegate.didTapCell(movieVM)
+        }
+    }
+    
 }
