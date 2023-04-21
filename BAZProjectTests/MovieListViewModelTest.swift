fe389c2976b0743e8a0ddd7082921ced2a49f774
Juan Manuel García Carmona <juan.garciaca@elektra.com.mx>
2023-04-21 02:14:46 -0600

    Se agregan peticiones al API real

--- BAZProjectTests/MovieListViewModelTest.swift
+++ BAZProjectTests/MovieListViewModelTest.swift
@@ -15,14 +15,14 @@ final class MovieListViewModelTest: XCTestCase {
     override func setUp() {
         super.setUp()
 
-        self.sut = MovieListViewModel(.trending)
+        sut = MovieListViewModel(.trending)
         let moveListFake = try? JSONDecoder().decode(MovieList.self, from: MovieFakes().trending)
         sut.setMovies(moveListFake?.results ?? [])
     }
     
     override func tearDown() {
         super.tearDown()
-        self.sut = nil
+        sut = nil
     }
     
     func testMovieList_whenReadFromMock_checkNumberOfTreandingMovies() {
@@ -82,6 +82,76 @@ final class MovieListViewModelTest: XCTestCase {
         let result = sut.numberOfRowsInSection(0)
 
         XCTAssertEqual(result, resultExpected)
+    }
+    
+    func testMoveList_whenReadFromAPI_checkNumberOfNowPlayingMovies() {
+        let resultExpected = 20
+        
+        let expectation = XCTestExpectation(description: "Obteniendo resultados del filtro Now Playing")
+        
+        sut.applyMovieFilter(.nowPlaying) { [weak self] in
+            XCTAssertEqual(self?.sut.numberOfRowsInSection(0), resultExpected)
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: 5)
+
+    }
+    
+    func testMoveList_whenReadFromAPI_checkNumberOfPopularMovies() {
+        let resultExpected = 20
+        
+        let expectation = XCTestExpectation(description: "Obteniendo resultados del filtro Popular")
+        
+        sut.applyMovieFilter(.popular) { [weak self] in
+            XCTAssertEqual(self?.sut.numberOfRowsInSection(0), resultExpected)
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: 5)
+
+    }
+    
+    func testMoveList_whenReadFromAPI_checkNumberOfTopRatedMovies() {
+        let resultExpected = 20
+        
+        let expectation = XCTestExpectation(description: "Obteniendo resultados del filtro Top Rated")
+        
+        sut.applyMovieFilter(.topRated) { [weak self] in
+            XCTAssertEqual(self?.sut.numberOfRowsInSection(0), resultExpected)
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: 5)
+
+    }
+    
+    func testMoveList_whenReadFromAPI_checkNumberOfUpcomingMovies() {
+        let resultExpected = 20
+        
+        let expectation = XCTestExpectation(description: "Obteniendo resultados del filtro Upcoming")
+        
+        sut.applyMovieFilter(.upcoming) { [weak self] in
+            XCTAssertEqual(self?.sut.numberOfRowsInSection(0), resultExpected)
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: 5)
+
+    }
+    
+    func testMoveList_whenReadFromAPI_checkNumberOfTrendingMovies() {
+        let resultExpected = 20
+        
+        let expectation = XCTestExpectation(description: "Obteniendo resultados del filtro Treanding")
+        
+        sut.applyMovieFilter(.trending) { [weak self] in
+            XCTAssertEqual(self?.sut.numberOfRowsInSection(0), resultExpected)
+            expectation.fulfill()
+        }
+
+        wait(for: [expectation], timeout: 5)
+
     }
 
 }
