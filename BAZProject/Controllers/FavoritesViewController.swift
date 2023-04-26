30d8ded8968b569cd417c93162194f117a07f76e
Juan Manuel García Carmona <juan.garciaca@elektra.com.mx>
2023-04-26 00:10:59 -0600

    Se elimina comentario

--- BAZProject/Controllers/FavoritesViewController.swift
+++ BAZProject/Controllers/FavoritesViewController.swift
@@ -12,14 +12,4 @@ final class FavoritesViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 }
