d9aff3af09f525f257b53335469c2849ccfc2989
Juan Manuel García Carmona <juan.garciaca@elektra.com.mx>
2023-04-26 01:32:17 -0600

    Se elimina espacio

--- BAZProject/Views/MovieTableViewCell.swift
+++ BAZProject/Views/MovieTableViewCell.swift
@@ -30,7 +30,6 @@ final class MovieTableViewCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
-    
 }
 
 extension MovieTableViewCell {
