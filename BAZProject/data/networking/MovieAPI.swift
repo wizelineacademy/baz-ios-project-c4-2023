50e8d7283b830df13b712798bbc8fc23b558f217
IVAN TECPANECATL MARTINEZ <ivan.tecpanecatl@elektra.com.mx>
2023-05-03 16:38:20 -0600

    implementacion de pruebas unitarias para la clase SearchDataSource

--- BAZProject/data/networking/MovieAPI.swift
+++ BAZProject/data/networking/MovieAPI.swift
@@ -9,57 +9,6 @@ import Foundation
 class MovieAPI {
 
     /**
-     Servicio que consume de la API DB movie
-     - Parameter query: Palabra a consultar
-     - Parameter completion: El closure a llamar una vez que se completa la operación.
-                             El closure toma un parámetro `[Movie]` que devolvera al terminar la peticion.
-     */
-    func searchMoviesForKeyword(_ query: String, completion: @escaping ([Movie]) -> Void) {
-        let url = Consts.END_POINTS.SEARCH_MOVIES.setParameters(old: "{keyword}", new: query).toUrl()
-        URLSession.shared.dataTask(with: .init(url: url)) { data, response, error in
-            var movies: [Movie] = []
-            defer {
-                completion(movies)
-            }
-            guard let data = data else { return }
-
-            do {
-                let decoder = JSONDecoder()
-                let response = try decoder.decode(MoviesResponse.self, from: data)
-                movies = response.results
-            } catch {
-                print("\(#function) Error decoding JSON: \(error)")
-            }
-        }.resume()
-    }
-
-    /**
-     Servicio que consume de la API DB movie
-     - Parameter query: Palabra a consultar
-     - Parameter completion: El closure a llamar una vez que se completa la operación.
-                             El closure toma un parámetro `[Movie]` que devolvera al terminar la peticion.
-     */
-    func searchForKeyword(_ query: String, completion: @escaping ([String]) -> Void) {
-        let url = Consts.END_POINTS.SEARCH_FOR_KEYWORD.setParameters(old: "{keyword}", new: query).toUrl()
-        URLSession.shared.dataTask(with: .init(url: url)) { data, response, error in
-            var keywords: [String] = []
-            defer {
-                completion(keywords)
-            }
-            guard let data = data,
-                  let json = try? JSONSerialization.jsonObject(with: data) as? NSDictionary,
-                  let results = json.object(forKey: "results") as? [NSDictionary]
-            else { return }
-
-            for result in results {
-                if let name = result.object(forKey: "name") as? String {
-                    keywords.append(name)
-                }
-            }
-        }.resume()
-    }
-
-    /**
      Metodo generico para consumir peticiones
      */
     func request<T: Decodable>(url: URL, completion: @escaping ([T]) -> Void) {
--- BAZProject/data/repositories/SearchDataSourceRemote.swift
+++ BAZProject/data/repositories/SearchDataSourceRemote.swift
@@ -10,7 +10,6 @@ import Foundation
 // MARK: - Class
 class SearchDataSourceRemote: SearchRepository {
 
-    private let movieApi = MovieAPI()
 
     /**
      Esta funcion implementa el metodo del protocolo SearchRepository
@@ -20,21 +19,52 @@ class SearchDataSourceRemote: SearchRepository {
      - Parameter completion: El closure de finalización que toma una matriz de objetos Movie como argumento de entrada y no devuelve nada.
      */
     func searchMovies(query: String, completion: @escaping ([Movie]) -> Void) {
-        movieApi.searchMoviesForKeyword(query) { movies in
-            completion(movies)
+        DispatchQueue.global(qos: .background).async {
+            let url = Consts.END_POINTS.SEARCH_MOVIES.setParameters(old: "{keyword}", new: query).toUrl()
+            URLSession.shared.dataTask(with: .init(url: url)) { data, response, error in
+                var movies: [Movie] = []
+                defer {
+                    completion(movies)
+                }
+                guard let data = data else { return }
+
+                do {
+                    let decoder = JSONDecoder()
+                    let response = try decoder.decode(MoviesResponse.self, from: data)
+                    movies = response.results
+                } catch {
+                    print("\(#function) Error decoding JSON: \(error)")
+                }
+            }.resume()
         }
     }
 
     /**
      Esta funcion implementa el metodo del protocolo SearchRepository
-     Obtiene todas las peliculas relacionadas a la palabra clave que se recibe en la variable query y llama al closure de finalización como argumento con los objetos Movie correspondientes.
+     Obtiene todas las peliculas relacionadas a la palabra clave que se recibe en la variable query y llama al closure de finalización como argumento con el arreglo de Strings correspondientes.
 
      - Parameter query: Palabra clave para buscar cualquier pelicula en base a la palabra clave.
-     - Parameter completion: El closure de finalización que toma una matriz de objetos Movie como argumento de entrada y no devuelve nada.
+     - Parameter completion: El closure de finalización que toma una matriz de Strings como argumento de entrada y no devuelve nada.
      */
     func searchKeyword(query: String, completion: @escaping ([String]) -> Void) {
-        movieApi.searchForKeyword(query) { keywords in
-            completion(keywords)
+        DispatchQueue.global(qos: .background).async {
+            let url = Consts.END_POINTS.SEARCH_FOR_KEYWORD.setParameters(old: "{keyword}", new: query).toUrl()
+            URLSession.shared.dataTask(with: .init(url: url)) { data, response, error in
+                var keywords: [String] = []
+                defer {
+                    completion(keywords)
+                }
+                guard let data = data,
+                      let json = try? JSONSerialization.jsonObject(with: data) as? NSDictionary,
+                      let results = json.object(forKey: "results") as? [NSDictionary]
+                else { return }
+
+                for result in results {
+                    if let name = result.object(forKey: "name") as? String {
+                        keywords.append(name)
+                    }
+                }
+            }.resume()
         }
     }
 }
