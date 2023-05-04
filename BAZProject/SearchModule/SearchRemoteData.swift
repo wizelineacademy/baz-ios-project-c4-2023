7ec8730b9d345ac71c32fd1838507069825d9a91
Grecia Escarcega <grecia.escarcega@bancoazteca.com>
2023-05-04 10:44:59 -0600

    Se corrige identaciión

--- BAZProject/SearchModule/SearchRemoteData.swift
+++ BAZProject/SearchModule/SearchRemoteData.swift
@@ -13,8 +13,8 @@ class SearchRemoteData {
     
     func searchMedia(_ searchText: String) async throws -> [MediaDataObject]? {
         let endpoint = SearchEndpoint.keyword(searchText)
-            let data = try await requestHandler.get(endpoint)
-            return try GenericResponseDataObject(data: data)?.results
-        }
+        let data = try await requestHandler.get(endpoint)
+        return try GenericResponseDataObject(data: data)?.results
+    }
     
 }
