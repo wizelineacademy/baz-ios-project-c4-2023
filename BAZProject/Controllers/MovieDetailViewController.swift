a80e7d8e8363233d28de74f05735a7ccdadfe82b
Juan Manuel García Carmona <juan.garciaca@elektra.com.mx>
2023-05-03 22:18:34 -0600

    Se pinta la imagen, titulo, afinidad, año y resumen de la película en la vista de detalle

--- BAZProject/Controllers/MovieDetailViewController.swift
+++ BAZProject/Controllers/MovieDetailViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 // MARK: - MovieDetailViewController
 final class MovieDetailViewController: UIViewController {
     
+    // MARK: - Properties
+    
     var movieVM: MovieViewModel?
-
+    private let imageLoader: ImageLoader = ImageLoader()
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var lblVote: UILabel!
+    @IBOutlet weak var lblYear: UILabel!
+    @IBOutlet weak var lblOverview: UILabel!
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         configureNavigationBar(largeTitleColor: .white, backgoundColor: UIColor(named: "background") ?? .white, tintColor: .white, title: movieVM?.title ?? "Detail", preferredLargeTitle: true)
+        
+        setupView()
     }
+}
+
+extension MovieDetailViewController {
+
+    func setupView() {
+        
+        lblTitle.text = movieVM?.title
+        lblOverview.text = movieVM?.overview
+        lblVote.text = movieVM?.vote_average
+        lblYear.text = movieVM?.year
+        
+        if let url = URL(string: movieVM?.poster_path ?? "") {
+            imageLoader.getImage(from: url) { [weak self] image in
+                self?.imgView.image = image
+            }
+        }
+    }
+    
 }
