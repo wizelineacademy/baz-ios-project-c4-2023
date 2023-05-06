7f3e2fe368eab444ce5257ae2ebae97a776a0c82
Juan Manuel García Carmona <juan.garciaca@elektra.com.mx>
2023-05-05 20:21:59 -0600

    Se agregan las propiedades overview, release_date, popularity y vote_average

--- BAZProject/Models/Movie.swift
+++ BAZProject/Models/Movie.swift
@@ -17,8 +17,12 @@ struct MovieList: Decodable {
 /// - id: An optional integer that represents the movie's unique identifier.
 /// - title: An optional string that represents the movie's title.
 /// - poster_path: An optional string that represents the movie's poster path.
+/// - overview: An optional string that represents overview of the movie's plot.
+/// - release_date: An optional string that represents the release date of the movie.
+/// - popularity: An optional string that represents the popularity score of the movie.
+/// - vote_average: An optional string that represents the average rating score of the movie.
 struct Movie: Decodable {
-    let id: Int?
+    let id: Int
     let title: String?
     let poster_path: String?
     let overview: String?
