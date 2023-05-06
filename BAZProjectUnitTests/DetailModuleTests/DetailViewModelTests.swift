c2e46cf9620fcd251ee3e605b46d416e5e1a1ad4
Grecia Escarcega <grecia.escarcega@bancoazteca.com>
2023-05-06 17:28:16 -0600

    se pasan diccionarios esperados a clase stub

--- BAZProjectUnitTests/DetailModuleTests/DetailViewModelTests.swift
+++ BAZProjectUnitTests/DetailModuleTests/DetailViewModelTests.swift
@@ -108,8 +108,7 @@ final class DetailViewModelTests: XCTestCase {
         let dataObject = try PersonDetailDataObject(data: DataStubs().personDetailData)!
         setViewModel(mediaItem: DataStubs.singlePersonMediaData)
         remoteData.personDetails = dataObject
-        let expectedDictionary: [DetailSection: [AnyHashable]] = [.overview: [OverviewModel(largeTitle: "Scott MacDonald", smallSubtitle: "Birth: 1/1/59", image: "/pBXNUejSwDHfWCD9GgmvRbIPBdb.jpg", description: "biography", defaultImage: nil)],
-                                                                  .filmography: [MediaItem(id: 27318, posterPath: "/sXOATwGluNbb9ezfG6DvXCiXvJp.jpg", title: "Jack Frost", rating: 5.14, mediaType: .movie, releaseDate: DateFormatter.getDate(from: "1997-11-18"))]]
+        let expectedDictionary = DataStubs.personDetailDictionary
         let person = try await sut.getPersonDetails(id: 12)!
         let dictionary = sut.buildDictionary(from: person)
 
@@ -122,13 +121,7 @@ final class DetailViewModelTests: XCTestCase {
         let dataObject = try MovieDetailDataObject(data: DataStubs().movieDetailData)!
         setViewModel(mediaItem: DataStubs.singleMovieMediaData)
         remoteData.movieDetails = dataObject
-        let expectedDictionary: [DetailSection: [AnyHashable]] =
-            [.overview: [OverviewModel(largeTitle: "The Fifth Element", smallSubtitle: "1997", image: "/fPtlCO1yQtnoLHOwKtWz7db6RGU.jpg", description: "fifthElement", defaultImage: "poster")],
-             .cast: [DetailCastMember(actor: MediaItem(id: 62, posterPath: "/caX3KtMU42EP3VLRFFBwqIIrch5.jpg", title: "Bruce Willis", rating: nil, mediaType: .person, releaseDate: nil), character: "Korben Dallas")],
-             .similar: [MediaItem(id: 2486, posterPath: "/k8f8WZjTzZ6TJYrKpyayCB4zKlw.jpg", title: "Eragon", rating: 5.066, mediaType: .movie, releaseDate: DateFormatter.getDate(from: "2006-12-14"))],
-             .recommended: [MediaItem(id: 607, posterPath: "/uLOmOF5IzWoyrgIF5MfUnh5pa1X.jpg", title: "Men in Black", rating: 7.18, mediaType: .movie, releaseDate: DateFormatter.getDate(from: "1997-07-02"))],
-             .reviews: [DetailReview(author: "GeekMasher", content: "best movies", date: DateFormatter.getDate(from: "2021-06-23T15:57:20.936Z", format: "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"))]
-            ]
+        let expectedDictionary = DataStubs.movieDetailDictionary
         let movie = try await sut.getMovieDetails(id: 12)!
         let dictionary = sut.buildDictionary(from: movie)
 
@@ -144,17 +137,7 @@ final class DetailViewModelTests: XCTestCase {
         let dataObject = try TVDetailDataObject(data: DataStubs().tvDetailData)!
         setViewModel(mediaItem: DataStubs.singleTVMediaData)
         remoteData.tvDetails = dataObject
-        let expectedDictionary: [DetailSection: [AnyHashable]] =
-            [.overview: [OverviewModel(largeTitle: "W*A*L*T*E*R", smallSubtitle: "1984", image: "/fwSw4fl08MVl5w6Q9lVUGIFkZQ2.jpg", description: "wally", defaultImage: "poster")],
-             
-             .cast: [DetailCastMember(actor: MediaItem(id: 89547, posterPath: "/3klT2K1UGl1wvmHKuDcSPuUFasV.jpg", title: "Brian Dobson", rating: nil, mediaType: .person, releaseDate: nil), character: "Ben Grimm (voice)")],
-             
-             .similar: [MediaItem(id: 87415, posterPath: "/pah40fuJA2tZBcY1rhvpSlj0Ys1.jpg", title: "Shadow", rating: 6.4, mediaType: .tv, releaseDate: DateFormatter.getDate(from: "2019-03-08"))],
-             
-             .recommended: [MediaItem(id: 11294, posterPath: "/10XpD4aEOExWcOg9bxCA28NNsMQ.jpg", title: "Kitchen Nightmares", rating: 7.48, mediaType: .tv, releaseDate: DateFormatter.getDate(from: "2007-09-19"))],
-             
-                .reviews: [DetailReview(author: "Peter89Spencer", content: "liked", date: DateFormatter.getDate(from: "2021-06-23T15:58:47.976Z", format: "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"))]
-            ]
+        let expectedDictionary = DataStubs.tvDetailDictionary
         let tv = try await sut.getTVDetails(id: 12)!
         let dictionary = sut.buildDictionary(from: tv)
 
--- BAZProjectUnitTests/TestDoubles/DataStubs.swift
+++ BAZProjectUnitTests/TestDoubles/DataStubs.swift
@@ -41,4 +41,26 @@ class DataStubs {
         return try! Data(contentsOf: file)
     }
     
+    static let personDetailDictionary: [DetailSection: [AnyHashable]] = [.overview: [OverviewModel(largeTitle: "Scott MacDonald", smallSubtitle: "Birth: 1/1/59", image: "/pBXNUejSwDHfWCD9GgmvRbIPBdb.jpg", description: "biography", defaultImage: nil)],
+                                                                  .filmography: [MediaItem(id: 27318, posterPath: "/sXOATwGluNbb9ezfG6DvXCiXvJp.jpg", title: "Jack Frost", rating: 5.14, mediaType: .movie, releaseDate: DateFormatter.getDate(from: "1997-11-18"))]]
+    static let movieDetailDictionary: [DetailSection: [AnyHashable]] =
+    [.overview: [OverviewModel(largeTitle: "The Fifth Element", smallSubtitle: "1997", image: "/fPtlCO1yQtnoLHOwKtWz7db6RGU.jpg", description: "fifthElement", defaultImage: "poster")],
+     .cast: [DetailCastMember(actor: MediaItem(id: 62, posterPath: "/caX3KtMU42EP3VLRFFBwqIIrch5.jpg", title: "Bruce Willis", rating: nil, mediaType: .person, releaseDate: nil), character: "Korben Dallas")],
+     .similar: [MediaItem(id: 2486, posterPath: "/k8f8WZjTzZ6TJYrKpyayCB4zKlw.jpg", title: "Eragon", rating: 5.066, mediaType: .movie, releaseDate: DateFormatter.getDate(from: "2006-12-14"))],
+     .recommended: [MediaItem(id: 607, posterPath: "/uLOmOF5IzWoyrgIF5MfUnh5pa1X.jpg", title: "Men in Black", rating: 7.18, mediaType: .movie, releaseDate: DateFormatter.getDate(from: "1997-07-02"))],
+     .reviews: [DetailReview(author: "GeekMasher", content: "best movies", date: DateFormatter.getDate(from: "2021-06-23T15:57:20.936Z", format: "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"))]
+    ]
+    
+    static let tvDetailDictionary: [DetailSection: [AnyHashable]] =
+    [.overview: [OverviewModel(largeTitle: "W*A*L*T*E*R", smallSubtitle: "1984", image: "/fwSw4fl08MVl5w6Q9lVUGIFkZQ2.jpg", description: "wally", defaultImage: "poster")],
+     
+     .cast: [DetailCastMember(actor: MediaItem(id: 89547, posterPath: "/3klT2K1UGl1wvmHKuDcSPuUFasV.jpg", title: "Brian Dobson", rating: nil, mediaType: .person, releaseDate: nil), character: "Ben Grimm (voice)")],
+     
+     .similar: [MediaItem(id: 87415, posterPath: "/pah40fuJA2tZBcY1rhvpSlj0Ys1.jpg", title: "Shadow", rating: 6.4, mediaType: .tv, releaseDate: DateFormatter.getDate(from: "2019-03-08"))],
+     
+     .recommended: [MediaItem(id: 11294, posterPath: "/10XpD4aEOExWcOg9bxCA28NNsMQ.jpg", title: "Kitchen Nightmares", rating: 7.48, mediaType: .tv, releaseDate: DateFormatter.getDate(from: "2007-09-19"))],
+     
+        .reviews: [DetailReview(author: "Peter89Spencer", content: "liked", date: DateFormatter.getDate(from: "2021-06-23T15:58:47.976Z", format: "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"))]
+    ]
+    
 }
