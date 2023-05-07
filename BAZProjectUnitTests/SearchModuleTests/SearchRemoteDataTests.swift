f16b4f0c380adc8efdf33c9e3f9ea044697abd76
Grecia Escarcega <grecia.escarcega@bancoazteca.com>
2023-05-07 14:38:44 -0600

    Se agregan pruebas a searchRemoteData

--- BAZProjectUnitTests/SearchModuleTests/SearchRemoteDataTests.swift
+++ BAZProjectUnitTests/SearchModuleTests/SearchRemoteDataTests.swift
@@ -10,7 +10,7 @@ import XCTest
 
 final class SearchRemoteDataTests: XCTestCase {
 
-    func test_SearchMedia_GetMockedItems() async throws {
+    func test_SearchMedia_GetMockedItemsViaMedia() async throws {
         let requestSession = RequestSessionMock()
         requestSession.data = DataStubs.completeData
         let requestHandler = RequestHandlerMock(urlSession: requestSession)
@@ -21,5 +21,41 @@ final class SearchRemoteDataTests: XCTestCase {
         
         XCTAssertEqual(items, expectedItems)
     }
-
+    
+    
+    func test_SearchMedia_GetMockedItemsViaMovies() async throws {
+        let requestSession = RequestSessionMock()
+        requestSession.data = DataStubs.completeData
+        let requestHandler = RequestHandlerMock(urlSession: requestSession)
+        let searchRemote = SearchRemoteData(requestHandler: requestHandler)
+        let expectedItems: [MediaDataObject]? = [MediaDataObject(id: 3465, title: "df_SAWERQ", posterPath: "path//")]
+        
+        let items = try await searchRemote.searchMovies("")
+        
+        XCTAssertEqual(items, expectedItems)
+    }
+    
+    func test_SearchMedia_GetMockedItemsViaSeries() async throws {
+        let requestSession = RequestSessionMock()
+        requestSession.data = DataStubs.completeData
+        let requestHandler = RequestHandlerMock(urlSession: requestSession)
+        let searchRemote = SearchRemoteData(requestHandler: requestHandler)
+        let expectedItems: [MediaDataObject]? = [MediaDataObject(id: 3465, title: "df_SAWERQ", posterPath: "path//")]
+        
+        let items = try await searchRemote.searchSeries("")
+        
+        XCTAssertEqual(items, expectedItems)
+    }
+    
+    func test_SearchMedia_GetMockedItemsViaPeople() async throws {
+        let requestSession = RequestSessionMock()
+        requestSession.data = DataStubs.completeData
+        let requestHandler = RequestHandlerMock(urlSession: requestSession)
+        let searchRemote = SearchRemoteData(requestHandler: requestHandler)
+        let expectedItems: [MediaDataObject]? = [MediaDataObject(id: 3465, title: "df_SAWERQ", posterPath: "path//")]
+        
+        let items = try await searchRemote.searchPeople("")
+        
+        XCTAssertEqual(items, expectedItems)
+    }
 }
