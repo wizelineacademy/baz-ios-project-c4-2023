2379a578eba6bcf4876b3951ff8a35d44d59bc80
Juan Manuel García Carmona <juan.garciaca@elektra.com.mx>
2023-05-07 05:53:36 -0600

    Se da funcionalidad a FavoritesViewController para use pinte los datos de local

--- BAZProject/Controllers/FavoritesViewController.swift
+++ BAZProject/Controllers/FavoritesViewController.swift
@@ -7,9 +7,38 @@
 
 import UIKit
 
-final class FavoritesViewController: UITableViewController {
+final class FavoritesViewController: MasterMovieTableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Configure the navigation bar
+        configureNavigationBar(largeTitleColor: .white, backgoundColor: UIColor(named: "background") ?? .white, tintColor: .white, title: "Favorite Movies", preferredLargeTitle: true)
+        
+        self.setupViewModel(MovieListViewModel(.favorites))
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        MovieListLocal().getMovies() { [weak self] movieList in
+            self?.viewModel?.setMovies(movieList.results)
+        }
+    }
+}
+
+extension FavoritesViewController: MasterMovieTableViewProtocols {
+    
+    /// Sets up the view model for the movies view controller.
+    /// - Parameters:
+    ///     - viewModel: The view model to be set up for the movies view controller.
+    func setupViewModel(_ viewModel: MovieListViewModel) {
+        viewModel.delegate = self
+        self.viewModel = viewModel
+    }
+}
+
+extension FavoritesViewController: MovieListViewModelProtocol {
+    func reloadData() {
+        tableView.reloadData()
     }
 }
