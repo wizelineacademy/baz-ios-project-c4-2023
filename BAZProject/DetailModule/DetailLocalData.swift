7382b7166656a0537e7bc0eb8cc6c717aade43b4
Grecia Escarcega <grecia.escarcega@bancoazteca.com>
2023-05-08 13:16:59 -0600

    Se modifica archivo local para delegarle codificado y decodificado de variables

--- BAZProject/DetailModule/DetailLocalData.swift
+++ BAZProject/DetailModule/DetailLocalData.swift
@@ -9,26 +9,64 @@ import Foundation
 
 class DetailLocalData {
     
-    private var userDefaultsManager: UserDefaultsProtocol
+    private var userDefaults: UserDefaultsProtocol
+    private var encoder: JSONEncoder
+    private var decoder: JSONDecoder
     
-    init(userDefaultsManager: UserDefaultsProtocol = UserDefaults.standard) {
-        self.userDefaultsManager = userDefaultsManager
+    init(userDefaults: UserDefaultsProtocol = UserDefaults.standard, encoder: JSONEncoder = JSONEncoder(), decoder: JSONDecoder = JSONDecoder()) {
+        self.userDefaults = userDefaults
+        self.encoder = encoder
+        self.decoder = decoder
+    }
+
+    
+    func getExistingItems(key: String) throws -> [MediaItem]? {
+        if let existingData = userDefaults.getData(forKey: key) {
+            return try decoder.decode([MediaItem].self, from: existingData)
+        }
+        return nil
     }
     
-    func save(data: Data, key: String) {
-        userDefaultsManager.save(data, forKey: key)
+    func favouriteExistsIn(_ existingItems: [MediaItem], item: MediaItem) -> Bool {
+        for existingItem in existingItems {
+            if existingItem.id == item.id && existingItem.title == item.title && existingItem.releaseDate == item.releaseDate && existingItem.mediaType == existingItem.mediaType {
+                return true
+            } else {
+                continue
+            }
+        }
+        return false
     }
     
-    func findItem(for key: String) -> Bool {
-        return userDefaultsManager.find(key: key)
+    func saveOrDeleteFavourite(item: MediaItem) throws {
+        var encodedData: Data?
+        if let existingElements = try getExistingItems(key: "Favourites") {
+            var elements = existingElements
+            if !favouriteExistsIn(existingElements, item: item) {
+                elements.append(item)
+            } else {
+                elements.removeAll(where: { $0 == item})
+            }
+            encodedData = try encoder.encode(elements)
+        } else {
+            encodedData = try encoder.encode([item])
+        }
+        userDefaults.save(encodedData, forKey: "Favourites")
     }
     
-    func deleteItem(for key: String) {
-        userDefaultsManager.delete(for: key)
+    func saveRecentlySearched(_ item: MediaItem) throws {
+        if let existingElements = try getExistingItems(key: "Recent") {
+            var elements = existingElements
+            if !favouriteExistsIn(existingElements, item: item) {
+                elements.append(item)
+                let data = try encoder.encode(elements)
+                userDefaults.save(data, forKey: "Recent")
+            }
+        } else {
+            let encoded = try encoder.encode([item]) 
+            userDefaults.save(encoded, forKey: "Recent")
+        }
     }
     
-    func getItem(for key: String) -> Data? {
-        userDefaultsManager.getData(forKey: key)
-    }
     
 }
