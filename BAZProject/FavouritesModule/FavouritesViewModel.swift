aedf47a8b04d82b214a4ff18c56d4224c2de61c1
Grecia Escarcega <grecia.escarcega@bancoazteca.com>
2023-05-08 11:04:59 -0600

    Se agrega viewModel para favoritos Se agrega testing para viewModel favoritos

---
+++ BAZProject/FavouritesModule/FavouritesViewModel.swift
@@ -0,0 +1,56 @@
+//
+//  FavouritesViewModel.swift
+//  BAZProject
+//
+//  Created by gescarcega on 08/05/23.
+//
+
+import UIKit
+
+class FavouritesViewModel {
+    
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, MediaItem>
+    
+    private var localData: FavouritesLocalData
+    private var error: Box<Error?> = Box(nil)
+    private var snapshot = Box(Snapshot())
+    
+    init(localData: FavouritesLocalData) {
+        self.localData = localData
+    }
+    
+    func bindError(_ listener: @escaping (Error?) -> Void) {
+        error.bind(listener)
+    }
+    
+    func bindSnapshot(_ listener: @escaping (Snapshot) -> Void) {
+        snapshot.bind(listener)
+    }
+    
+    func loadFavourites() {
+        if let favourites = getLocalData() {
+            var auxSnapshot = Snapshot()
+            auxSnapshot.appendSections([0])
+            auxSnapshot.appendItems(favourites, toSection: 0)
+            snapshot.value = auxSnapshot
+        }
+    }
+    
+    func removeFavourite(_ item: MediaItem) {
+        do {
+            try localData.removeFavourite(item)
+            snapshot.value.deleteItems([item])
+        } catch {
+            self.error.value = error
+        }
+    }
+    
+    func getLocalData() -> [MediaItem]? {
+        do {
+            return try localData.getFavourites()
+        } catch {
+            self.error.value = error
+            return  nil
+        }
+    }
+}
---
+++ BAZProjectUnitTests/FavouritesModuleTests/FavouritesViewModelTests.swift
@@ -0,0 +1,94 @@
+//
+//  FavouritesViewModelTests.swift
+//  BAZProjectUnitTests
+//
+//  Created by gescarcega on 08/05/23.
+//
+
+import XCTest
+@testable import BAZProject
+
+final class FavouritesViewModelTests: XCTestCase {
+    
+    func test_getLocalData_ShouldReturnMockedData() throws {
+        let decoder = DecoderSpy()
+        let udMock = UserDefaultsMock()
+        let data = [MediaItem(id: 1), MediaItem(id: 2), MediaItem(id: 3)]
+        udMock.data = try JSONEncoder().encode(data)
+        let local = FavouritesLocalData(udManager: udMock, decoder: decoder)
+        let sut = FavouritesViewModel(localData: local)
+        
+        let retrievedData = sut.getLocalData()
+        
+        XCTAssertEqual(retrievedData, data)
+    }
+    
+    func test_getLocalData_ShouldEncounterError() throws {
+        let decoder = DecoderSpy()
+        let expectedError = NSError(domain: "MammaMia!", code: -123)
+        decoder.error = expectedError
+        let udMock = UserDefaultsMock()
+        let data = [MediaItem(id: 1), MediaItem(id: 2), MediaItem(id: 3)]
+        udMock.data = try JSONEncoder().encode(data)
+        let local = FavouritesLocalData(udManager: udMock, decoder: decoder)
+        let sut = FavouritesViewModel(localData: local)
+        let expectation = XCTestExpectation()
+        var errorHolder: NSError?
+        
+        let _ = sut.getLocalData()
+        sut.bindError { error in
+            errorHolder = error as? NSError
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 0.5)
+        
+        XCTAssertEqual(errorHolder, expectedError)
+    }
+    
+    func test_loadFavourites_ShouldReturnMockedData() throws {
+        let decoder = DecoderSpy()
+        let udMock = UserDefaultsMock()
+        let data = [MediaItem(id: 1), MediaItem(id: 2), MediaItem(id: 3)]
+        udMock.data = try JSONEncoder().encode(data)
+        let local = FavouritesLocalData(udManager: udMock, decoder: decoder)
+        let sut = FavouritesViewModel(localData: local)
+        let expectation = XCTestExpectation()
+        var snapshotHolder = NSDiffableDataSourceSnapshot<Int, MediaItem>()
+        
+        sut.loadFavourites()
+        sut.bindSnapshot() { snapshot in
+            snapshotHolder = snapshot
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 0.5)
+
+        XCTAssertEqual(snapshotHolder.numberOfItems, data.count)
+    }
+    
+    func test_removeFavourite_ShouldRemoveFavouriteFromSnapshotAndUDManager() throws {
+        let decoder = DecoderSpy()
+        let udMock = UserDefaultsMock()
+        let item1 = MediaItem(id: 1)
+        var data = [MediaItem(id: 2), MediaItem(id: 3)]
+        data.append(item1)
+        udMock.data = try JSONEncoder().encode(data)
+        let local = FavouritesLocalData(udManager: udMock, decoder: decoder)
+        let sut = FavouritesViewModel(localData: local)
+        let expectation = XCTestExpectation()
+        var snapshotHolder = NSDiffableDataSourceSnapshot<Int, MediaItem>()
+        
+        sut.loadFavourites()
+        sut.removeFavourite(item1)
+        sut.bindSnapshot { snapshot in
+            snapshotHolder = snapshot
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 0.5)
+        let udData = sut.getLocalData()
+
+        XCTAssertEqual(snapshotHolder.numberOfItems, data.count - 1)
+        XCTAssertNil(snapshotHolder.sectionIdentifier(containingItem: item1))
+        XCTAssertFalse(udData?.contains(item1) ?? true)
+    }
+    
+}
