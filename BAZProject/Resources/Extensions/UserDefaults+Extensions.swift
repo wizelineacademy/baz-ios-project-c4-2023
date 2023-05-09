152ec8648f81ed6601c5d5f49cf968d9f3c703d0
Juan Manuel García Carmona <juan.garciaca@elektra.com.mx>
2023-05-08 18:33:35 -0600

    se elimina el force unwrapping de la suit y se deja como opcional,

--- BAZProject/Resources/Extensions/UserDefaults+Extensions.swift
+++ BAZProject/Resources/Extensions/UserDefaults+Extensions.swift
@@ -10,7 +10,7 @@ import Foundation
 /// A shared instance of `UserDefaults` with a specific suite name.
 /// - Returns: A `UserDefaults` instance with the suite name "mx.caffeina.bazproject".
 extension UserDefaults {
-    static var shared: UserDefaults {
-        return UserDefaults(suiteName: "mx.caffeina.bazproject")!
+    static var shared: UserDefaults? {
+        return UserDefaults(suiteName: "mx.caffeina.bazproject")
     }
 }
