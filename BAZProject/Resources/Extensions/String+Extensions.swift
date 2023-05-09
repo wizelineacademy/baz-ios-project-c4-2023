fc7bbe3691e58c7635d984cbed94b965a79457b6
Juan Manuel García Carmona <juan.garciaca@elektra.com.mx>
2023-05-08 18:36:45 -0600

    Se agrega espacio para guard

--- BAZProject/Resources/Extensions/String+Extensions.swift
+++ BAZProject/Resources/Extensions/String+Extensions.swift
@@ -22,6 +22,7 @@ internal extension String {
     /// - Returns: An NSAttributedString object if conversion succeeds, or nil if an error occurs during conversion.
     var htmlToAttributedString: NSAttributedString? {
         guard let data = data(using: .utf8) else { return nil }
+        
         return try? NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding:String.Encoding.utf8.rawValue], documentAttributes: nil)
     }
 }
